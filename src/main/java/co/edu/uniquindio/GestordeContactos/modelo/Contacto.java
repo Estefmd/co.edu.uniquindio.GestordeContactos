72454cabaeaa27c3b2dc6f4a72bb28ba25767b2a
Estefmd <142418723+Estefmd@users.noreply.github.com>
2024-09-28 19:57:30 -0500

    Creacion Carpeta resources

--- src/main/java/co/edu/uniquindio/GestordeContactos/modelo/Contacto.java
+++ src/main/java/co/edu/uniquindio/GestordeContactos/modelo/Contacto.java
@@ -1,4 +1,21 @@
 package co.edu.uniquindio.GestordeContactos.modelo;
 
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+
 public class Contacto {
+
+    private String nombre;
+    private String apellido;
+    private String numTelefono;
+    private LocalDate fechaCumpleanios;
+    private String correoElectronico;
+    private String urlFoto;
+
 }
--- src/main/java/co/edu/uniquindio/GestordeContactos/modelo/GestorPrincipal.java
+++ src/main/java/co/edu/uniquindio/GestordeContactos/modelo/GestorPrincipal.java
@@ -1,4 +1,23 @@
 package co.edu.uniquindio.GestordeContactos.modelo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class GestorPrincipal {
+
+    private ArrayList<Contacto> contactos = new ArrayList<>();
+
+    public void agregarContacto(String nombre, String apellido, String numTelefono, LocalDate fechaCumpleanios,
+                                String correoElectronico, String urlFoto) throws Exception {
+        if(nombre.isBlank() || numTelefono.isBlank()){
+            throw new Exception("El nombre y el Numero de Telefono son obligatorios");
+        }
+        Contacto contacto = new Contacto(nombre,apellido,numTelefono,fechaCumpleanios,correoElectronico,urlFoto);
+        contactos.add(contacto);
+    }
+
+    public ArrayList<Contacto> listarContactos(){
+        return contactos;
+    }
+
 }
